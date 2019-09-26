fb555dc1b53d666d0c897378c8de22a249539976
Leonardo Motta <mail@leomotta.me>
2019-09-25 21:08:20 -0400

    feature:Add amount field to redux

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -41,7 +41,7 @@ export default connect(mapStateToProps)(function Cart({ cart }) {
                   <button type="button">
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                  <input type="number" readOnly value={1} />
+                  <input type="number" readOnly value={product.amount} />
                   <button type="button">
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
