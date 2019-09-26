c5776e4be2eece0467b2fbabe80e52eeeb8dc855
Leonardo Motta <mail@leomotta.me>
2019-09-25 20:55:20 -0400

    feature:add amount baged from redux to cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
-export default function Header() {
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -16,10 +19,10 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cartSize} itens</span>
         </div>
         <MdShoppingBasket size={36} color="#fff" />
       </Cart>
     </Container>
   );
-}
+});
