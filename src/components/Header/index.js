63443ceda95718d3745aeeaab2a3585aafea1891
Leonardo Motta <mail@leomotta.me>
2019-10-16 12:15:35 -0300

    refactor: add redux useSelector hook to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
 import { Container, Cart } from './styles';
 import logo from '../../assets/images/logo.svg';
 
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -25,4 +25,4 @@ export default connect(state => ({
       </Cart>
     </Container>
   );
-});
+}
